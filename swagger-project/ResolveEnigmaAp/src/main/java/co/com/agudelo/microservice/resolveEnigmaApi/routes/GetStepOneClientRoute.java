aa466f2499a9e727109b50f403f99a8951fce756
VaAgudelo17 <114451968+VaAgudelo17@users.noreply.github.com>
2024-03-13 01:48:05 -0500

    Add files via upload
    second commit

---
+++ swagger-project/ResolveEnigmaAp/src/main/java/co/com/agudelo/microservice/resolveEnigmaApi/routes/GetStepOneClientRoute.java
@@ -0,0 +1,15 @@
+package co.com.agudelo.microservice.resolveEnigmaApi.routes;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.builder.RouteBuilder;
+
+public class GetStepOneClientRoute extends RouteBuilder  {
+
+	@Override
+	public void configure() throws Exception {
+		
+		
+	}
+	
+
+}
